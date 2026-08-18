891270ed9bc85c73bdf5ec50531ef4637803af07
hasanhabib16011998 <hasan16habib@gmail.com>
2026-08-18 23:59:38 +0600

    deploy: fixed network issue

--- sun-re/vite.config.js
+++ sun-re/vite.config.js
@@ -10,5 +10,11 @@ export default defineConfig({
   server: {
     host: true,
     port: 5174,
+    allowedHosts: true,
+  },
+  preview: {
+    host: true,
+    port: 5174,
+    allowedHosts: true,
   },
 })
