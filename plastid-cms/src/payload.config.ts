41ac49b2484f125a34b9199836982dfe4138d628
hasanhabib16011998 <hasan16habib@gmail.com>
2026-09-21 19:15:26 +0600

    deploy:cms push database issue

--- plastid-cms/src/payload.config.ts
+++ plastid-cms/src/payload.config.ts
@@ -71,6 +71,7 @@ export default buildConfig({
     pool: {
       connectionString: process.env.DATABASE_URI || '',
     },
+    push: true,
   }),
 
   // ── Storage — MinIO (S3-compatible) ───────────────────────────────────────
